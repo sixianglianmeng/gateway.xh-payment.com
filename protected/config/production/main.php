3701922cd20a6eee94ae1af88e86f5537513ee79
bootmall <booter.ui@gmail.com>
2018-06-05 21:35:39 +0800

    配置生产环境

--- protected/config/production/main.php
+++ protected/config/production/main.php
@@ -9,7 +9,7 @@ $config = \yii\helpers\ArrayHelper::merge(
                 'class' => 'yii\db\Connection',
                 'dsn' => 'mysql:host=127.0.0.1;dbname=payment_com',
                 'username' => 'payment_com',
-                'password' => '3MLNH3tatiXSKzr8',
+                'password' => 'z4wIxIJtgH7wouBO',
                 'charset' => 'utf8',
                 'tablePrefix' => 'p_',
                 'enableSchemaCache' => true,
@@ -26,9 +26,9 @@ $config = \yii\helpers\ArrayHelper::merge(
             ],
         ],
         'params' => [
-            'domain.cdn' => 't1.agent.huaruipay.com',
-            'domain.gateway.rpc' => 't1.gateway.huaruipay.com',
-            'corsOriginDomain' => ['*','t1.gateway.huaruipay.com'],
+            'domain.cdn' => 'portal-api.huaruipay.com',
+            'domain.gateway.rpc' => 'gateway.i.huaruipay.com',
+            'corsOriginDomain' => ['*','portal-api.huaruipay.com'],
         ],
     ]
 );
