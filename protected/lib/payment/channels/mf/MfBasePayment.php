d4e9a2dbf43d2113f8b796d9a2573f744feb737f
bootmall <bootmall@gmail.com>
2018-10-25 21:48:57 +0800

    秒付

--- protected/lib/payment/channels/mf/MfBasePayment.php
+++ protected/lib/payment/channels/mf/MfBasePayment.php
@@ -142,7 +142,7 @@ class MfBasePayment extends BasePayment
 
         $params['sign'] = self::md5Sign($params,$this->paymentConfig['key']);
 
-        $requestUrl = $this->paymentConfig['gateway_base_uri'].'/order.html';
+        $requestUrl = $this->paymentConfig['gateway_base_uri'].'/api/v1/order-sync';
         $resTxt = self::post($requestUrl,$params,[],25);
         //接口日志记录
         LogicApiRequestLog::rechargeAddLog($this->order, $requestUrl, $resTxt, $params);
